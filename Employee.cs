d3aea6290eec879777622f6e112fe4e24ae8ac51
sodiqoladeji <sodiqoladeji69@gmail.com>
2024-12-22 05:51:45 -0800

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondProject
+{
+    internal class Employee
+    {
+        public int EmployeeId;
+        private string EmployeeCat;
+        public int PayRate;
+        public double BasicPay;
+        public double Deductions;
+        public int Wage;
+        public int NumberOfHours;
+        public int NumberOfContracts;
+        public Employee(int employeeId, string employeeCat, double basicPay, double deductions)
+        { 
+            EmployeeId = employeeId;
+            EmployeeCat = employeeCat;
+            BasicPay = basicPay;
+            Deductions = deductions;
+        }
+        public Employee(int employeeId, string employeeCat, int wage,int numberofhours, double deductions) 
+        {
+            this.EmployeeId = employeeId;
+            this.EmployeeCat = employeeCat;
+            this.Wage = wage;
+            this.NumberOfHours = numberofhours;
+            this.Deductions = deductions;
+        }
+        public Employee(int employeeId, string employeeCat, double basicPay, int numberofcontracts, double deductions)
+        {
+            EmployeeId = employeeId;
+            EmployeeCat = employeeCat;
+            BasicPay = basicPay;
+            NumberOfContracts = numberofcontracts;
+            Deductions = deductions;
+        }
+        public string EmployeeCategory
+        {
+            get { return EmployeeCat; }
+            set
+            {
+                if(value == "FullTimeEmployee" ||  value == "PartTimeEmployee" || value == "Contractor")
+                    EmployeeCat = value;
+            }
+
+        }
+        public double Pay() 
+        {
+            if (EmployeeCat == "FullTimeEmployee") 
+            {
+              Console.WriteLine((BasicPay * 1) - Deductions);
+                
+            }
+            
+            else if (EmployeeCat == "PartTimeEmployee")
+            { 
+                Console.WriteLine((Wage * NumberOfHours) - Deductions);
+            }
+
+            else
+            {
+                Console.WriteLine((BasicPay * NumberOfContracts) - Deductions);
+            }
+
+            return Pay();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SecondProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee01 = new Employee(01, "FullTimeEmployee", 2800, 198);
+            var employee02 = new Employee(02, "Contractor", 5000, 3, 1700);
+            var employee03 = new Employee(03, "PartTimeEmployee", 15, 65, 82);
+
+            Console.WriteLine(employee01.Pay());
+            Console.WriteLine(employee02.Pay());
+            Console.WriteLine(employee03.Pay());
+            
+            Console.ReadLine();
+        }
+    }
+}
