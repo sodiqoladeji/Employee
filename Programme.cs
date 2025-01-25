d6049a7a97a1bfb0df3fe9bfbfdfdd84e18921bc
sodiqoladeji <sodiqoladeji69@gmail.com>
2025-01-25 14:16:32 -0800

    Add files via upload

--- Programme.cs
+++ Programme.cs
@@ -1,26 +1,138 @@
-﻿namespace Exeercise3
+﻿using System.Diagnostics;
+
+namespace Exeercise3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] nums = {2,7,11,13,15 };
-            int target = 20;
 
-            
+            //Plus Minus...
+            int[] arr = { -1, -2,0, 3, 4, 5 };
+            double pos = 0;
+            double neg = 0;
+            double zer = 0;
+            int n = arr.Length;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    pos++;
+                }
+                else if (arr[i] < 0)
+                {
+                    neg++;
+                }
+                else if (arr[i] == 0)
+                {
+                    zer++;
+                }
+
+
+            }
+
+            Console.WriteLine((pos / n));
+            Console.WriteLine((neg / n));
+            Console.WriteLine((zer / n));
+
+
+
+            //Compare the Triplets...
+            int[] a = [10, 25, 70];
+            int[] b = [15, 9, 40];
+            int[] c = [0, 0];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    c[0]++;
+
+                }
+                else if (a[i] < b[i])
+                {
+                    c[1]++;
+                }
+                else if (a[i] == b[i])
+                {
+
+                }
+
+
+            }
+            Console.WriteLine(c[0] + "," + c[1]);
+
+
+
+
+            //Simple Array Sum...
+            int[] nums = [6, 7, 5, 3, 4, 9];
+            int result = 0;
             for (int i = 0; i < nums.Length; i++)
             {
-                for (int j = 0; j < nums.Length; j++)
+
+                result = result + nums[i];
+
+
+
+            }
+            Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            int[] num = { 2, 7, 11, 13, 15 };
+            int target = 20;
+
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                for (int j = 0; j < num.Length; j++)
                 {
-                    int n = target;
-                    if (i!=j && nums[i] + nums[j] == n )
+                    int na = target;
+                    if (i != j && num[i] + num[j] == na)
                     {
                         Console.WriteLine("[" + i + "]" + "," + "[" + j + "]");
                     }
-                    
+
 
                 }
-                
+
             }
 
 
@@ -36,16 +148,16 @@
             for (int i = 0; i <= w2.Length; i++)
             {
 
-                int n = 1;
-                if (w2[i] == w2[w2.Length - n])
+                int ni = 1;
+                if (w2[i] == w2[w2.Length - ni])
                 {
-                    n++;
+                    ni++;
                     Console.WriteLine("This word is a palindrome");
 
                     break;
 
                 }
-                else if (w2[i] != w2[w2.Length - n])
+                else if (w2[i] != w2[w2.Length - ni])
                 {
                     Console.WriteLine("This word is not a palindrome");
                     break;
@@ -68,14 +180,14 @@
             {
 
 
-                int n;
+                int nu;
                 for (int i = 1; i < n1; i++)
                 {
                     if (n1 % i == 0 && n2 % i == 0 && i != 1)
                     {
-                        n = i;
+                        nu = i;
 
-                        Console.WriteLine("The LCM of " + n1 + " and " + n2 + " is " + n);
+                        Console.WriteLine("The LCM of " + n1 + " and " + n2 + " is " + nu);
                         break;
                     }
 
