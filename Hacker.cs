94143358ceea6568b33a102e71d9b5fc9f1b0946
sodiqoladeji <sodiqoladeji69@gmail.com>
2025-01-30 12:35:46 -0800

    Add files via upload

---
+++ Hacker.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace HackerRank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Anagram...
+            string word1 = "lemon";
+            char[] a = [];
+            a = word1.ToCharArray();
+
+            string word2 = "melon";
+            char[] b = [];
+            b = word2.ToCharArray();
+            int c =0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if (a[i] == b[j] )
+                    {
+                        c++; 
+                        break;
+                    }
+                    
+                }
+                continue;
+                
+
+            }
+            if (c == a.Length)
+            {
+                Console.WriteLine("Word is an Anagram");
+            }
+            else
+            {
+                Console.WriteLine("Word is not an Anagram");
+            }
+        }
+    }
+}
